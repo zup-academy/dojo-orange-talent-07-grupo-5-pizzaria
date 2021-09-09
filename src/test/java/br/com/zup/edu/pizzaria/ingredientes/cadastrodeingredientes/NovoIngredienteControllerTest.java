0e6de5a76aa1aaccea981e51ae84db3def5df7fa
Kleysson Silva <kleysson.silva@zup.com.br>
2021-09-09 16:11:06 -0300

    Piloto Arthur

--- src/test/java/br/com/zup/edu/pizzaria/ingredientes/cadastrodeingredientes/NovoIngredienteControllerTest.java
+++ src/test/java/br/com/zup/edu/pizzaria/ingredientes/cadastrodeingredientes/NovoIngredienteControllerTest.java
@@ -1,25 +1,19 @@
 package br.com.zup.edu.pizzaria.ingredientes.cadastrodeingredientes;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import javax.transaction.Transactional;
-
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -43,7 +37,21 @@ class NovoIngredienteControllerTest {
         mvc.perform(request)
            .andExpect(status().isCreated())
            .andExpect(header().exists("Location"))
-                .andExpect(redirectedUrlPattern("/api/ingredientes/\\d"));
+                .andExpect(redirectedUrlPattern("/api/ingredientes/*"));
+
+    }
+
+    @Test
+    void naoDeveCadastrarNovoIngredienteInvalido() throws Exception {
+
+        NovoIngredienteRequest body = new NovoIngredienteRequest("Queijo muçarela", new BigDecimal("2.0"), 0);
+        MockHttpServletRequestBuilder request = post("/api/ingredientes")
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(new ObjectMapper().writeValueAsString(body));
+
+        mvc.perform(request)
+           .andExpect(status().isBadRequest())
+                .andDo(print());
 
     }
 }
